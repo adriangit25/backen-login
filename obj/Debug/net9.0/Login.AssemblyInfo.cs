0b3fcb12b26ee72a92f4037e6ce20a64e406d063
adriangit25 <adrenher2468@gmail.com>
2025-04-27 15:08:11 -0500

    Resolucion del problema de registro conjunto

--- obj/Debug/net9.0/Login.AssemblyInfo.cs
+++ obj/Debug/net9.0/Login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e801a7de931289deea7b9ebb0479d578976fae9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e062374e8488ccd21f75032ff26fc00bb6056234")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
