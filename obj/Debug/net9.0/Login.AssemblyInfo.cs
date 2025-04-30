4a17223a91ac68412a978258ad1855741aeffbfe
adriangit25 <adrenher2468@gmail.com>
2025-04-30 02:03:27 -0500

    Cambio de RAMA

--- obj/Debug/net9.0/Login.AssemblyInfo.cs
+++ obj/Debug/net9.0/Login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd457f4986a0cb882a88e943d0457846725d45b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+048c12011908f6f41489147d29aaf9300f6d4cfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
