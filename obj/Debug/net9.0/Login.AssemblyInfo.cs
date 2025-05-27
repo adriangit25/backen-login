dd64fc119567ab7d624d37f7151204b4df4c8e7e
Adrian Hermosa <adrenher2468@gmail.com>
2025-05-26 21:51:37 -0500

    Rol Dinamico

--- obj/Debug/net9.0/Login.AssemblyInfo.cs
+++ obj/Debug/net9.0/Login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d56447f0ea911bb1672e6091720df7b9dfea3ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d00657c36ea2545fefdde1161fd0e098e48b127")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
